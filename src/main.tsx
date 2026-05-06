01118873a297c2daa09783ddbf159338f883e484
Rodrigo Cardoso adm <rodrigocra@C02FFJ5PMD6M.local>
2026-05-06 11:50:25 +0100

    erro

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Routes from './routes'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Routes />
   </StrictMode>,
 )
