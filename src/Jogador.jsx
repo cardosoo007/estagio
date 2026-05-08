d883289a7912376486df1684f528ef23a636bb83
Rodrigo Cardoso adm <rodrigocra@C02FFJ5PMD6M.local>
2026-05-08 12:03:32 +0100

    adicionar página jogador e click

---
+++ src/Jogador.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function Jogador() {
+    const [jogador, setJogador] = useState([])
+    useEffect(() => {
+
+        fetch("/api/marcadores/jogador")
+            .then(response => response.json())
+            .then(data => {
+                setJogador(data)
+            })
+
+    }, []);
+    console.log(jogador)
+    return (
+        <div>
+            <h1>Detalhes do Jogador</h1>
+            <p>Nome: {jogador.nome}</p>
+            <p>Equipa: {jogador.equipa}</p>
+            <p>Golos: {jogador.golos}</p>
+
+        </div>
+    );
+}
+
+export default Jogador;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import Routes from "./routes";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Routes from './Routes.jsx'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Routes />
   </StrictMode>,
-);
+)
