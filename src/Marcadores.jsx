3015bf1ab55fe6ab2eecf8ec76fd2954dee054dc
Ricardo Almeida <ricardosaul25@gmail.com>
2026-05-08 12:20:26 +0100

    Fix.

--- src/Marcadores.jsx
+++ src/Marcadores.jsx
@@ -1,34 +1,29 @@
-import { cursorTo } from "node:readline";
 import { useEffect, useState } from "react";
 
 function Marcadores() {
-    const [listaMarcadores, setListaMarcadores] = useState([])
-    useEffect(() => {
+  const [listaMarcadores, setListaMarcadores] = useState([]);
+  useEffect(() => {
+    fetch("/api/marcadores")
+      .then((response) => response.json())
+      .then((data) => {
+        setListaMarcadores(data);
+      });
+  }, []);
+  console.log(listaMarcadores);
+  return (
+    <div>
+      <h1>Marcadores</h1>
+      <p>melhores marcadores</p>
 
-        fetch("/api/marcadores")
-            .then(response => response.json())
-            .then(data => {
-                setListaMarcadores(data)
-            })
-
-    }, []);
-    console.log(listaMarcadores)
-    return (
-        <div>
-            <h1>Marcadores</h1>
-            <p>melhores marcadores</p>
-
-            <ul>
-                {listaMarcadores.map(marcador => (
-                    <li key={marcador.jogador}
-                        style={{ cursor: "pointer" }}
-                    >
-                        {marcador.jogador} - {marcador.golos}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+      <ul>
+        {listaMarcadores.map((marcador) => (
+          <li key={marcador.jogador} style={{ cursor: "pointer" }}>
+            {marcador.jogador} - {marcador.golos}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Marcadores;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Routes from './Routes.jsx'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Routes from "./Routes.jsx";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Routes />
   </StrictMode>,
-)
+);
