2e164b7b186542e37752aae69385dd8ae31f745e
Rodrigo USER <rodrigoc@GGHMHNHX7W-PT001740.local>
2026-06-08 10:02:57 +0100

    implementaçao de uma nova pagina de login

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router';
+import { Avatar, Button } from '@chakra-ui/react';
 
 // Layout comum da app: menu de navegação e localização do conteúdo.
 function Layout() {
+  const loggedIn = true;
   return (
     <div>
       <nav className="navbar">
@@ -15,6 +17,17 @@ function Layout() {
         <Link to="/marcadores">Marcadores</Link>
         <Link to="/torneios">Torneios</Link>
         <Link to="/configuracoes">Configurações</Link>
+        <div className="login-button">
+          {loggedIn ? (
+            <Avatar.Root>
+              <Avatar.Fallback name="Rodrigo Cardoso" />
+            </Avatar.Root>
+          ) : (
+            <Link to="/login">
+              <Button colorPalette="green">Login</Button>
+            </Link>
+          )}
+        </div>
       </nav>
       {/* Aqui o componente da rota atual é renderizado */}
       <Outlet />
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -14,6 +14,7 @@ import Jogadores from './Jogadores.jsx';
 import Admin from './admin/Admin';
 import AdminJogador from './admin/AddJogador';
 import Layout from './Layout.jsx';
+import Login from './Login.jsx';
 
 // Roteador principal da aplicação.
 // Define as rotas e qual componente renderiza para cada caminho.
@@ -34,6 +35,7 @@ const AppRouter = () => {
           <Route element={<Admin />} path="/admin" />
           <Route element={<AdminJogador />} path="/admin/adjogador" />
           <Route element={<Jogador />} path="/jogadores/:id" />
+          <Route element={<Login />} path="/login" />
           <Route element={<App />} path="/" />
         </Route>
       </Routes>
