1925c966412a4bf0cc6f7f1b9e1180087796f16f
Rodrigo USER <rodrigoc@GGHMHNHX7W-PT001740.local>
2026-06-09 11:33:28 +0100

    trocar estrutura para a indicada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import Liga5 from '../liga5.png';
 
 // Componente principal da página de entrada do site.
 // Mostra apenas um título de boas-vindas para a aplicação.
 function App() {
   return (
     <div className="pagina-principal">
-      <h1>Liga 5</h1>
+      <img className="logo" src={Liga5} alt="Liga5" />
     </div>
   );
 }
