ba67826a31704c24d1bc22cfe0a96d31ba16fd62
Rodrigo USER <rodrigoc@GGHMHNHX7W-PT001740.local>
2026-06-26 13:25:32 +0100

    integrar react query devtools

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from './components/ui/provider';
 import './index.css';
 import Routes from './Routes.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,7 @@ createRoot(document.getElementById('root')).render(
           <Routes />
         </Provider>
       </Auth0Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
