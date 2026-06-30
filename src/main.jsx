b081f33ffc8078d2a400d1bf7375ed103a2487ba
Rodrigo USER <rodrigoc@GGHMHNHX7W-PT001740.local>
2026-06-30 17:05:22 +0100

    validaçao de conta adm por auth0

--- src/main.jsx
+++ src/main.jsx
@@ -27,7 +27,7 @@ createRoot(document.getElementById('root')).render(
           <Routes />
         </Provider>
       </Auth0Provider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </StrictMode>,
 );
